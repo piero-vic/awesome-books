69d324ee81fe073ac1d0a5301a1aac1408b1d0f0
Carlos-SSH <carlos.anezeh@me.com>
2021-10-18 14:37:03 -0500

    Add a function called getInput that contains ID's from html inputs.

---
+++ index.js
@@ -0,0 +1,9 @@
+let library = [];
+
+function getInput() {
+  const book = {};
+  book.title = document.getElementById("bookTitle").value;
+  book.author = document.getElementById("bookAuthor").value;
+  return book;
+}
+
