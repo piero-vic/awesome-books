70a3f236866c2e7efa287e11474587b87b67eab4
Carlos-SSH <carlos.anezeh@me.com>
2021-10-18 15:32:50 -0500

    Implement LocalStorage.

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ function getInput() {
 function addBook(bookObj) {
   const bookList = document.getElementById('book-list');
   const book = document.createElement("LI");
-  book.innerHTML = bookObj.title;
+  book.innerHTML = `Title: ${bookObj.title} <br> Author: ${bookObj.author}`;
   bookList.appendChild(book);
 }
 
@@ -18,5 +18,18 @@ const addButton = document.getElementById('add-btn')
 addButton.addEventListener('click', function(){
   book = getInput()
   library.push(book);
-  addBook(book)
+  addBook(book);
+  localStorage.setItem('library', JSON.stringify(library));
 })
+
+window.onload = function(){
+  library = JSON.parse(localStorage.getItem('library' || '[]'));
+  if (library === null){
+    library = [];
+    return
+  }
+
+  library.forEach((book) => {
+    addBook(book);
+  })
+}
