c7b2ded1852c01c34f1e160181cec2815102a23a
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-10-18 14:48:47 -0500

    Add addBook function and create list

--- index.js
+++ index.js
@@ -4,6 +4,12 @@ function getInput() {
   const book = {};
   book.title = document.getElementById("bookTitle").value;
   book.author = document.getElementById("bookAuthor").value;
-  return book;
+  library.push(book);
 }
 
+function addBook(bookObj) {
+    const bookList = document.getElementById('book-list')
+    const book = document.createElement("LI");
+    book.innerHTML = bookObj.title
+    bookList.appendChild(book)
+}
