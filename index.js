8cd77e3a1dedd17d6c35c968634628647ccb71ae
Piero Lescano <71156605+piero-vic@users.noreply.github.com>
2021-10-18 15:06:33 -0500

    Add button function

--- index.js
+++ index.js
@@ -4,12 +4,19 @@ function getInput() {
   const book = {};
   book.title = document.getElementById("bookTitle").value;
   book.author = document.getElementById("bookAuthor").value;
-  library.push(book);
+  return book
 }
 
 function addBook(bookObj) {
-    const bookList = document.getElementById('book-list')
-    const book = document.createElement("LI");
-    book.innerHTML = bookObj.title
-    bookList.appendChild(book)
+  const bookList = document.getElementById('book-list');
+  const book = document.createElement("LI");
+  book.innerHTML = bookObj.title;
+  bookList.appendChild(book);
 }
+
+const addButton = document.getElementById('add-btn')
+addButton.addEventListener('click', function(){
+  book = getInput()
+  library.push(book);
+  addBook(book)
+})
